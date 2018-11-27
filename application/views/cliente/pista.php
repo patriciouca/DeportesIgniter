e69b863e692f0f09615e5863fa47130ad31651c4
patriciouca <patricio.fernandez.florez@alum.uca.es>
2018-11-27 10:30:20 +0100

    alquiler

--- application/views/cliente/pista.php
+++ application/views/cliente/pista.php
@@ -13,16 +13,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <?php
 
 
-        $fecha= new DateTime("now");
+    $fecha= new DateTime("now");
 
 
-        $fecha_actual = $fecha->format('Y-m-d');
+    $fecha_actual = $fecha->format('Y-m-d');
 
-        $fecha->add(new DateInterval('P7D'));
-        $semSig= $fecha->format('Y-m-d');
-
-
-
+    $fecha->add(new DateInterval('P7D'));
+    $semSig= $fecha->format('Y-m-d');
     ?>
     <br>
     <input id="calendario" type="date" name="fecha" value="<?= $fecha_actual ?>" min="<?= $fecha_actual ?>" max="<?= $semSig ?>">
@@ -30,6 +27,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <ul id="fechas">
         
     </ul>
+
+    <?php
+    echo form_open('Cliente/alquilar');
+
+    $data= array(
+            'id'=> 'decision',
+        'name' => 'fecha',
+        'class' => 'input_box',
+        'type' =>'text'
+    );
+    echo form_input($data);
+
+    $data = array(
+        'name' => 'alquilar',
+        'type' => 'submit',
+        'value'=> 'Alquilar',
+        'class'=> 'btn-primary'
+    );
+    echo form_submit($data);
+    ?>
+    <?php echo form_close();?>
  </div>
 
 
@@ -37,37 +55,112 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     var almacenar;
     var calendario=$('#calendario').val();
     var url=<?= "'".base_url()."cliente/disponibilidad/'" ?>;
-    
+    var url2=<?= "'".base_url()."cliente/horario/'" ?>;
+
     $( document ).ready(function() {
         peticionGet();
     });
 
     $('#buscar').on( "click", function() {
+        calendario=$('#calendario').val();
         if(almacenar!=calendario)
         {
             peticionGet();
         }
     });
-    
+
+    $('#fechas').on( "click","li", function(eventData,handler) {
+        if(eventData.target.className == "clickable bg-success")
+            $('#decision').val(formatoSegundo(eventData.target.id));
+    });
+
+    function pillada(hora,fecha){
+
+        for (var i=0;i<fecha.length;i++)
+        {
+            if(fecha[i].horaInicio==formatoSegundo(hora))
+                return true;
+        }
+        return false;
+
+    }
+
     function mostrarDia(fecha) {
         var i=0;
-        for (;i<24;i++)
-        {
-            $('#fechas').append("<li>"+formatoHora(i)+"</li>");
-        }
+        $('#fechas').html("");
+        var dia=diaSemana();
+        var horaIni1,horaIni2,horaFin1,horaFin2;
+
+        $.get(url2+dia, function( data ) {
+           horaIni1=data.horaInicioM;
+           horaIni2=data.horaInicioT;
+           horaFin1=data.horaFinM;
+           horaFin2=data.horaFinT;
+
+            for (;i<24;i++)
+            {
+                if(pillada(formatoHora(i),fecha))
+                    $('#fechas').append("<li class=\"bg-danger\">"+formatoHora(i)+"-"+formatoHora(i+1)+"</li>");
+                else
+                    $('#fechas').append("<li id=\""+formatoHora(i)+"\" class=\"clickable bg-success\">"+formatoHora(i)+"-"+formatoHora(i+1)+"</li>");
+
+            }
+        });
+
+
+
     }
     
     function peticionGet() {
+
         $.get(url+calendario, function( data ) {
             mostrarDia(data);
             almacenar=calendario;
         });
+
     }
 
     function formatoHora(hora) {
-        var digitos=hora.length;
+        var cadena=""+hora;
+        var digitos=cadena.length;
+
+        if(hora<10)
+        {
+            cadena="0"+cadena;
+            for(;digitos<3;digitos++)
+            {
+                if(digitos==1)
+                {
+                    cadena=cadena+":";
+                }
+                cadena=cadena+"0";
+            }
+
+        }
+        else{
+            for(;digitos<4;digitos++)
+            {
+                if(digitos==2)
+                {
+                    cadena=cadena+":";
+                }
+                cadena=cadena+"0";
+            }
+        }
+        return cadena;
     }
 
+    function formatoSegundo(fecha)
+    {
+        return fecha+=":00";
+    }
+
+    function diaSemana(){
+
+        var dias=[6, 0, 1, 2, 3, 4, 5];
+        var dt = new Date($('#calendario').val());
+        return dias[dt.getUTCDay()];
+    }
 </script>
 </body>
 </html>
